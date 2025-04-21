fde524ed88d0f2a56f808b9b307ae663f785ad72
Cliff Calist <cliffstersprog@gmail.com>
2025-04-21 23:05:34 +0200

    feat(common-ui): tab menu mechanic

---
+++ Runtime/CommonUI/TabMenu/TabButton.cs
@@ -0,0 +1,15 @@
+using R3;
+
+namespace WhiteArrow.MVVM.UI
+{
+    public class TabButton : ViewButton
+    {
+        private readonly Subject<Unit> _clicked = new();
+        public Observable<Unit> Clicked => _clicked;
+
+
+
+        protected override sealed void OnClicked() => _clicked.OnNext(Unit.Default);
+        public virtual void SetActive(bool isActive) => _object.SetActive(isActive);
+    }
+}
