6a26f4e052cfc431277d1f94d88f997542c4b6b7
Cliff Calist <cliffstersprog@gmail.com>
2025-04-23 15:34:14 +0200

    feat(common-ui): selector mechanic

---
+++ Runtime/CommonUI/Selector/Selector.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using System.Linq;
+using R3;
+using UnityEngine;
+
+namespace WhiteArrow.MVVM.UI
+{
+    public abstract class Selector : UIView
+    {
+        [SerializeField] private Transform _content;
+        [SerializeField] private SelectorOption _optionPrefab;
+
+        private List<SelectorOption> _options = new();
+
+        private ReactiveProperty<int> _selectedIndex = new(-1);
+        public ReadOnlyReactiveProperty<int> SelectedIndex => _selectedIndex;
+
+
+
+        public abstract int OptionsCount { get; protected set; }
+
+
+
+        protected override void BindFromCache()
+        {
+            UpdateOptionsCount();
+            UpdateOptionsIndexes();
+            UpdateOptionsStatus();
+        }
+
+
+
+        private void UpdateOptionsCount()
+        {
+            while (_options.Count < OptionsCount)
+            {
+                var newOption = Instantiate(_optionPrefab, _content);
+                OnOptionInstantiated(newOption);
+                _options.Add(newOption);
+
+                newOption.Clicked
+                    .Subscribe(OnOptionSelected)
+                    .AddTo(this);
+            }
+
+            while (_options.Count > OptionsCount)
+            {
+                var last = _options.LastOrDefault();
+                if (last == null)
+                    break;
+
+                _options.Remove(last);
+                OnOptionDestroyed(last);
+                Destroy(last.gameObject);
+            }
+        }
+
+        protected virtual void OnOptionInstantiated(SelectorOption option) { }
+        protected virtual void OnOptionDestroyed(SelectorOption option) { }
+
+
+
+        private void UpdateOptionsIndexes()
+        {
+            for (int i = 0; i < _options.Count; i++)
+                _options[i].SetIndex(i);
+        }
+
+        private void UpdateOptionsStatus()
+        {
+            for (int i = 0; i < _options.Count; i++)
+            {
+                var option = _options[i];
+                option.SetSelectedStatus(i == _selectedIndex.CurrentValue);
+            }
+        }
+
+
+
+        private void OnOptionSelected(int index)
+        {
+            _selectedIndex.Value = index;
+            UpdateOptionsStatus();
+        }
+    }
+}
---
+++ Runtime/CommonUI/Selector/SelectorOption.cs
@@ -0,0 +1,35 @@
+using System;
+using R3;
+
+namespace WhiteArrow.MVVM.UI
+{
+    public abstract class SelectorOption : ViewButton
+    {
+        protected int _index { get; private set; } = -1;
+
+        private readonly Subject<int> _clicked = new();
+        public Observable<int> Clicked => _clicked;
+
+
+
+
+        public void SetIndex(int index)
+        {
+            if (index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            _index = index;
+            RebindIfShowed();
+        }
+
+
+        protected override void OnClicked()
+        {
+            _clicked.OnNext(_index);
+        }
+
+
+
+        public abstract void SetSelectedStatus(bool isSelected);
+    }
+}
