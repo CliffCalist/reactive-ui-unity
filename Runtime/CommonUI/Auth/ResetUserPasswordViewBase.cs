93e9bf67147014a5eb3f73b7624819e92b3ce97a
Cliff Calist <cliffstersprog@gmail.com>
2025-10-29 19:57:26 +0100

    feat(auth): add ResetUserPasswordViewBase

---
+++ Runtime/CommonUI/Auth/ResetUserPasswordViewBase.cs
@@ -0,0 +1,25 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace WhiteArrow.ReactiveUI.Auth
+{
+    public abstract class ResetUserPasswordViewBase : AuthFormViewBase
+    {
+        [SerializeField] private TMP_InputField _inputEmail;
+
+
+
+        protected override sealed bool IsInputValid()
+        {
+            return AuthInputValidator.IsValidEmail(_inputEmail.text);
+        }
+
+        protected override sealed void PerformAction(Action<AuthOperationResult> callback)
+        {
+            SendResetPasswordEmail(_inputEmail.text, callback);
+        }
+
+        protected abstract void SendResetPasswordEmail(string email, Action<AuthOperationResult> callback);
+    }
+}
